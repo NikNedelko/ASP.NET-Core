8f7735d6073db2c2a2ed7516273f50eae3999f24
Nikita <nedelko1nik@gmail.com>
2022-01-12 20:53:54 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using PartyInvites.Models;
+using Partyinvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon"; return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // Обнаружена ошибка проверки достоверности, 
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => (bool)(r.WillAttend = true)));
+        }
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.ComponentModel.DataAnnotations;
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        // Пожалуйста, введите свое имя 
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\0.+\\..+",
+        ErrorMessage = "Please enter a valid email address")]
+        // Пожалуйста, введите свой адрес электронной почты 
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        // Пожалуйста, введите свой номер телефона 
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        // Пожалуйста, укажите, примете ли участие 
+        public bool? WillAttend { get; set; }
+
+    }
+}
